83ee07a813d26f030edbf661b24f76499ed1598e
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-25 16:23:39 -0400

    added edit page

--- app.rb
+++ app.rb
@@ -21,8 +21,18 @@ post '/pokemon' do
   redirect "/pokemon/#{@pokemon.id}"
 end
 
-
 get '/pokemon/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemon/show"
+end
+
+get '/pokemon/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemon/edit"
+end
+
+put '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemon/#{@pokemon.id}")
 end
